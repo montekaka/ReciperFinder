a5a3c5477bda58212e1871422f7ce67b4fc22bd6
montekaka <josh.chen.jia@gmail.com>
2015-09-27 21:56:49 -0700

    Add string escape

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,11 @@
 class RecipesController < ApplicationController
   def index
   	if params[:search]
-  		@search_term = params[:search].to_s
+  		@display_term = params[:search]
+  		@search_term = CGI.escape(@display_term)  		
   	else
   		@search_term = "chocolate"
+  		@display_term = @search_term
   	end   	
   	@recipes = Recipe.for(@search_term)
   end
