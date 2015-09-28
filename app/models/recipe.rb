75c1268b8eb40e384840af72e2d0ef1f951f0aba
montekaka <josh.chen.jia@gmail.com>
2015-09-27 21:06:33 -0700

    add sanitize

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe
 	include HTTParty
-	#key_value = ENV['FOOD2FORK_KEY']
-	#hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'	
-	#hostport = 'www.food2fork.com'
-	#base_uri "http://#{hostport}/api"
-	#default_params fields: "title,image_url", q: "search"
-	format :json
-
+	key_value = ENV['FOOD2FORK_KEY']
+	# hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'	
+	# base_uri "http://#{hostport}/api"
+	# default_params api_key:key_value,fields: "title,image_url", q: "search"
+	format :json		
+	
 	def self.for term
-		#get("", query: {query: term})["recipes"]
-		get("http://food2fork.com/api/search?key=#{ENV['FOOD2FORK_KEY']}&q=#{term}")["recipes"]
+		key_value = ENV['FOOD2FORK_KEY']
+		#get("", query: {query: term})["recipes"]	
+		get("http://food2fork.com/api/search?key=#{key_value}&q=#{term}")["recipes"]
 	end
 end
