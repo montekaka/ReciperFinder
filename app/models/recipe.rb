34087cc5729d2c4cb0dcd7895c723aeb814d5c24
montekaka <josh.chen.jia@gmail.com>
2015-09-27 22:47:51 -0700

    update api setting

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe
 	include HTTParty	
-	key_value = ENV['FOOD2FORK_KEY']
-	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'	
-	base_uri "http://#{hostport}/api"
-	default_params fields: "title,image_url,social_rank", q: "search"
+	# key_value = ENV['FOOD2FORK_KEY']
+	# hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'	
+	# base_uri "http://#{hostport}/api"
+	# default_params fields: "title,image_url,social_rank", q: "search"
 	format :json		
 	
 	def self.for term		
