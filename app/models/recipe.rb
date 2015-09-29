fffa2e80b5bc56b952cdeb2bb6b70dd8cd105541
montekaka <josh.chen.jia@gmail.com>
2015-09-28 21:06:17 -0700

    add a link to the title

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,12 @@
 class Recipe
 	include HTTParty	
-	# key_value = ENV['FOOD2FORK_KEY']
-	# hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'	
-	# base_uri "http://#{hostport}/api"
-	# default_params fields: "title,image_url,social_rank", q: "search"
+
+	def initialize()
+		@key_value = ENV['FOOD2FORK_KEY']
+		@hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'			
+	end
+	# base_uri "http://#{@hostport}/api"
+	# default_params api_key:@key_value, fields: "title,image_url,social_rank", q: "search"
 	format :json		
 	
 	def self.for term		
