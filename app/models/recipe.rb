4ed655be5921a2690e09b450b2bd4ed3cdf02aae
montekaka <josh.chen.jia@gmail.com>
2015-09-29 20:43:36 -0700

    fixed the api

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,14 @@
 class Recipe
 	include HTTParty	
-
-	def initialize()
-		@key_value = ENV['FOOD2FORK_KEY']
-		@hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'			
-	end
-	# base_uri "http://#{@hostport}/api"
-	# default_params api_key:@key_value, fields: "title,image_url,social_rank", q: "search"
+	key_value = ENV['FOOD2FORK_KEY']
+	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+	base_uri "http://#{hostport}/api"
+	default_params key:key_value
 	format :json		
 	
 	def self.for term		
-		#get("", query: {query: term})["recipes"]	
-		key_value = ENV['FOOD2FORK_KEY']
-		get("http://food2fork.com/api/search?key=#{key_value}&q=#{term}")["recipes"]
+		get("/search", query: {query: term})["recipes"]	
+		#key_value = ENV['FOOD2FORK_KEY']
+		#get("http://food2fork.com/api/search?key=#{key_value}&q=#{term}")["recipes"]
 	end
 end
